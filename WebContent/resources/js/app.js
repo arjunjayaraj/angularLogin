8dbdb0a24ae041c566c1805eabcc6e144d996a3e
Arjun k <k.arjunjayaraj@gmail.com>
2015-07-14 13:33:16 +0530

    initial commit

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -1,13 +1,7 @@
-var myapp = angular.module("myapp", []);
-myapp.controller("LoginController", function($scope) {
-    	$scope.user = {
-    		username: "",
-    		password: "",
-    		fullName:  function() {
-    	         var userObject;
-    	         userObject = $scope.user;
-    	        return userObject.username + " " + userObject.password;
-    	      }
-    	};
-            
-});
+(function(){
+	var myapp = angular.module("myapp", []);
+	myapp.controller("LoginController", function() {
+	            
+	})
+
+})();
