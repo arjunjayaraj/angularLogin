169af40308a66ae396de72f303ec09ea4a503fc1
Arjun k <k.arjunjayaraj@gmail.com>
2015-07-15 12:39:44 +0530

    partial spark integration

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -1,7 +1,30 @@
 (function(){
+
 	var myapp = angular.module("myapp", []);
-	myapp.controller("LoginController", function() {
-	            
-	})
+	myapp.controller('LoginController', ['$scope','$http',function($scope,$http) {
+		 console.log("The username is ",$scope.username);
+		 console.log("The username is ",$scope.password);
+	   $scope.login=function(){ $http({
+            method :'GET',
+            url:'http://localhost:8089/spark/',
+//            data: { username :  $scope.username , password: $scope.password},
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+            console.log('status',status);
+            console.log('data',status);
+            console.log('headers',status);
+        });};
+        myapp.controller('loginPanelCtrl', function($scope) {
+        	this.tab=1;
+        	this.selectTab=function(setTab){
+        		this.tab=setTab;
+        	};
+        	this.isSelected=function(checkTab){
+        		return this.tab===checkTab;
+        	};
+        });
+
+         
+	}])
 
 })();
