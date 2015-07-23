52a872bbe4379f073d237581fa6348f20742abb8
ashwinqb <ashwint@qburst.com>
2015-07-23 14:45:53 +0530

    commit

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -1,37 +1,42 @@
-(function(){
-	var user ={
-			username :  "arjun" ,
-			password: "arjun"			
-	}
-
+(function() {
+var test={
+		username : "arjun",
+		password : "arjun",
+		enabled: "false"
+		
+};
 	var myapp = angular.module("myapp", []);
 
-	myapp.controller('LoginController', ['$scope','$http',function($scope,$http) {
-		 console.log("The username is ",$scope.username);
-		 console.log("The username is ",$scope.password);
-	   $scope.login=function(){ $http({
-            method :'POST',
-            url:'http://localhost:8089/spark/logintest',
-            data:{username :  "arjun" ,
-    			password: "arjun" },
-            contentType: 'application/json; charset=utf-8'
-        }).success(function (data, status, headers, config) {
-            console.log('status',status);
-            console.log('the data is data',status);
-            console.log('headers',status);
-        });};
-                
-	}])
+	myapp.controller('LoginController', [ '$scope', '$http',
+			function($scope, $http) {
+				$scope.user = {
+					username : "arjun",
+					password : "arjun",
+				};
 
-	myapp.controller('TabController', function (){
+						$scope.login = function() {
+					$http({
+						method : 'POST',
+						url : 'http://localhost:8089/SurveyManagementSystem/reg',
+						params : test,
+						contentType : 'application/json; charset=utf-8'
+					}).success(function(data, status, headers, config) {
+						console.log('status', status);
+						console.log('the data is data', data);
+						console.log('headers', status);
+					});
+				};
+
+			} ]);
+
+	myapp.controller('TabController', function() {
 		this.tab = 1;
-		this.selectTab = function (setTab){
-		this.tab = setTab;
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
 		};
 		this.isSelected = function(checkTab) {
-		return this.tab === checkTab;
+			return this.tab === checkTab;
 		};
-		});
-
+	});
 
 })();
