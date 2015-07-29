63565761e5041dcae5e9bc6b43574a3995d2a7ae
Arjun k <k.arjunjayaraj@gmail.com>
2015-07-29 16:41:00 +0530

    sucessful login

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -8,23 +8,41 @@
 
 	var myapp = angular.module("myapp", [ 'ngCookies' ]).factory(
 			'XSRFInterceptor', function($cookies, $log) {
+				var gettokendata = function($httpProvider) {
+					var xhr = new XMLHttpRequest();
+					xhr.open('GET', 'http://localhost:8089/spark/test', false);
+					xhr.send();
+
+				};
 
 				var XSRFInterceptor = {
 
 					request : function(config) {
-
+						gettokendata();
 						var token = $cookies.get('XSRF-TOKEN');
-						
 
+						console.log("the token in cookie", token);
 						if (token) {
 							config.headers['X-CSRF-TOKEN'] = token;
+
 						}
+						config.headers['X-Requested-With'] = "XMLHttpRequest";
 
 						return config;
-					}
+					},
+					response : function(response) {
+						var tokentest1 = response.headers('X-CSRF-HEADER');
+						var tokentest2 = response.headers('X-CSRF-PARAM');
+						var tokentest3 = response.headers('X-CSRF-TOKEN');
+						console.log("the response token1 is", tokentest1);
+						console.log("the response token2 is", tokentest2);
+						console.log("the response token3 is", tokentest3);
+						return response;
+					},
 				};
 				return XSRFInterceptor;
 			});
+
 	myapp.config([ '$httpProvider', function($httpProvider) {
 
 		$httpProvider.defaults.withCredentials = true;
@@ -32,43 +50,98 @@
 
 	} ]);
 	console.log("in ajax function");
-	myapp.controller('LoginController', [ '$scope', '$http',
-			function($scope, $http) {
-				$scope.user = {
-						email : "",
-					fName : "fname",
-					lName : "lname",
-					password : ""
-				};
+	myapp
+			.controller(
+					'LoginController',
+					[
+							'$scope',
+							'$http',
+							function($scope, $http) {
+								$scope.user = {
+									j_username : "user",
+									j_password : "user"
+								};
 
-				console.log("The username is ", $scope.user.username);
-				console.log("The username is ", $scope.user.password);
-				$scope.register = function() {
+								console.log("The username is ",
+										$scope.user.username);
+								console.log("The username is ",
+										$scope.user.password);
+								//						
+								$scope.register = function() {
 
-					$http({
-						method : 'POST',
-						url : 'http://localhost:8089/Cart/registernew',
-						params : $scope.user,
-						contentType : 'application/json'
-					}).success(function(data, status, headers, config) {
-						
-					});
+									$http(
+											{
+												method : 'POST',
+												url : 'http://localhost:8089/spark/adduser',
+												params : $scope.user,
+												contentType : 'application/json'
+											}).success(
+											function(data, status, headers,
+													config) {
 
-				};
-				$scope.login = function() {
+											});
 
-					$http({
-						method : 'POST',
-						url : 'http://localhost:8089/Cart/j_spring_security_check',
-						params : $scope.user,
-						contentType : 'application/json'
-					}).success(function(data, status, headers, config) {
-						
-					});
+								};
+								$scope.test = function() {
 
-				};
+									$http(
+											{
+												method : 'GET',
+												url : 'http://localhost:8089/spark/test',
+												contentType : 'application/json'
+											}).success(
+											function(data, status, headers,
+													config) {
 
-			} ])
+											});
+
+								};
+
+								$scope.login = function() {
+
+									$http(
+											{
+												method : 'POST',
+												url : 'http://localhost:8089/spark/j_spring_security_check',
+												params : $scope.user,
+												contentType : 'application/json'
+											}).success(
+											function(data, status, headers,
+													config) {
+
+											}).error(
+											function(data, status, headers,
+													config) {
+												console.log("error header",
+														headers);
+												console.log("error config",
+														config);
+
+											});
+
+								};
+								$scope.logout = function() {
+									console.log("inside logout");
+
+									$http(
+											{
+												method : 'POST',
+												url : 'http://localhost:8089/spark/j_spring_security_logout',
+												contentType : 'application/json'
+											}).success(
+											function(data, status, headers,
+													config) {
+
+											}).error(
+											function(data, status, headers,
+													config) {
+												
+
+											});
+
+								};
+
+							} ])
 
 	myapp.controller('TabController', function() {
 		this.tab = 1;
--- WebContent/resources/js/test.js
+++ WebContent/resources/js/test.js
@@ -1,28 +1,108 @@
-var user ={
-			"username" :  "ashwin" ,
-			"password": "password"			
-	};
-var username ="arjun";
-var password = "arjun";
-angular.module('testApp.services',['ngResource']).factory('Entry', function($resource) {
-  return $resource('http://localhost:8089/spark/logintest'); // Note the full endpoint address
-});
 
+(function() {
+	var usertest = {
+			email : "testangular2@gmail.com",
+			fName : "fname",
+			lName : "lname",
+			password : "password"
+		};
+var myapp=angular.module('myapp', ['ngCookies'])
 
-angular.module('testApp.controllers',['testApp.services']).controller('ResourceController',function($scope, Entry) {
-  var entry ;
-  var post = $resource('http://localhost:8089/spark/logintest',{
-		   charge: {method:'post', params:{charge:true}}
-		  });
-  post.save(user);
-  this.login=function(){
-  $scope.entry = new Entry(); //You can instantiate resource class
- 
-  $scope.entry.data = { "username" :username,
-  		"password" :password,
-  };
- 
-  Entry.save($scope.entry, function() {
-    //data saved. do something here.
-  });} //saves an entry. Assuming $scope.entry is the Entry object  
+.run(['$http', '$cookies', function($http, $cookies) {
+  $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+}])
+
+.provider('myCSRF',[function(){
+  var headerName = 'X-CSRF-TOKEN';
+  var cookieName = 'XSRF-TOKEN';
+  var allowedMethods = ['GET'];
+
+  this.setHeaderName = function(n) {
+    headerName = n;
+  }
+  this.setCookieName = function(n) {
+    cookieName = n;
+  }
+  this.setAllowedMethods = function(n) {
+    allowedMethods = n;
+  }
+  this.$get = ['$cookies', function($cookies){
+    return {
+      'request': function(config) {
+        if(allowedMethods.indexOf(config.method) === -1) {
+          // do something on success
+          config.headers[headerName] = $cookies[cookieName];
+        }
+        return config;
+      }
+    }
+  }];
+}]).config(function($httpProvider) {
+  $httpProvider.interceptors.push('myCSRF');
 });
+myapp.controller('LoginController', [ '$scope', '$http',
+                          			function($scope, $http) {
+                          				$scope.user = {
+                          						email : "admin@gmail.com",
+                          					fName : "fname",
+                          					lName : "lname",
+                          					password : "123456"
+                          				};
+
+                          				console.log("The username is ", $scope.user.username);
+                          				console.log("The username is ", $scope.user.password);
+                          				$scope.register = function() {
+
+                          					$http({
+                          						method : 'POST',
+                          						url : 'http://localhost:8089/Cart/registernew',
+                          						params : $scope.user,
+                          						contentType : 'application/json'
+                          					}).success(function(data, status, headers, config) {
+                          						
+                          					});
+
+                          				};
+                          				$scope.test = function() {
+
+                          					$http({
+                          						method : 'GET',
+                          						url : 'http://localhost:8089/Cart/test',
+                          						contentType : 'application/json'
+                          					}).success(function(data, status, headers, config) {
+                          						
+                          					});
+
+                          				};
+                          				$scope.login = function() {
+
+                          					$http({
+                          						method : 'POST',
+                          						url : 'http://localhost:8089/Cart/j_spring_security_check',
+                          						params : $scope.user,
+                          						contentType : 'application/json'
+                          					}).success(function(data, status, headers, config) {
+                          						
+                          					}).error(function(data, status, headers, config) {
+                          						console.log("error header",headers);
+                          						console.log("error config",config);
+//                          						$scope.test();
+                          					});
+                          		
+
+                          				};
+                          				
+
+                          			} ])
+
+                          	myapp.controller('TabController', function() {
+                          		this.tab = 1;
+                          		this.selectTab = function(setTab) {
+                          			this.tab = setTab;
+                          		};
+                          		this.isSelected = function(checkTab) {
+                          			return this.tab === checkTab;
+                          		};
+                          	});
+
+                          })();
