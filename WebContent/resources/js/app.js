994bc7769f1d4b54f7e155117a104a03bb4d10ff
Arjun k <k.arjunjayaraj@gmail.com>
2015-08-10 13:56:49 +0530

    completed login function in ingnito window

--- WebContent/resources/js/app.js
+++ WebContent/resources/js/app.js
@@ -1,61 +1,42 @@
 (function() {
-	var access=false;
+	var access = false;
 
-	var myapp = angular.module("myapp", [ 'ngCookies','ngRoute']).factory(
-		
+	var myapp = angular.module("myapp", [ 'ngCookies', 'ngRoute' ]).factory(
 
-			'XSRFInterceptor', function($cookies, $log) {
-				var headerToken ;
-				var gettokendata = function($httpProvider) {
-					var xhr = new XMLHttpRequest();
-					xhr.open('GET', 'http://localhost:8089/spark/authenthicate', false);
-		
-					xhr.send();
-					headerToken =xhr.getResponseHeader('X-CSRF-TOKEN');
-					console.log("the header token is ss " ,headerToken);
-				};
-		
-			 
-				var XSRFInterceptor = {
-						
-					request : function(config) {
-						gettokendata();
-						var token = $cookies.get('XSRF-TOKEN');
+	'XSRFInterceptor', function($cookies, $log) {
 
-						console.log("the token in cookie", token);
-						if (token!=null) {
-							config.headers['X-CSRF-TOKEN'] = token;
+		var XSRFInterceptor = {
 
-						}
-						else{
-							config.headers['X-CSRF-TOKEN'] = headerToken;
-						}
-						config.headers['X-Requested-With'] = "XMLHttpRequest";
+			request : function(config) {
+				var token = $cookies.get('XSRF-TOKEN');
 
-						return config;
-					},
-					response : function(response) {
-						
+				console.log("the token in cookie in request", token);
+				if (token != null) {
+					config.headers['X-CSRF-TOKEN'] = token;
 
+				}
+				config.headers['X-Requested-With'] = "XMLHttpRequest";
 
-						return response;
-					},
-				};
+				return config;
+			},
+			response : function(response) {
 
-				return XSRFInterceptor;
+				return response;
+			},
+		};
 
-			});
-	myapp.run(['$rootScope',
-		      '$location',
-		      function ($rootScope, $location) {
-		          $rootScope.$on('$routeChangeStart', function (event, next) {
-		        	  if(next.access==true&&access == false){
-		        			$location.path('/login').replace();
+		return XSRFInterceptor;
 
-		        			}
+	});
+	myapp.run([ '$rootScope', '$location', function($rootScope, $location) {
+		$rootScope.$on('$routeChangeStart', function(event, next) {
+			if (next.access == true && access == false) {
+				$location.path('/login').replace();
 
-		          });
-		      }]);
+			}
+
+		});
+	} ]);
 
 	myapp.config([ '$httpProvider', function($httpProvider) {
 
@@ -64,18 +45,19 @@
 
 	} ]);
 	myapp.config(function($sceDelegateProvider) {
-		  $sceDelegateProvider.resourceUrlWhitelist([
-		    // Allow same origin resource loads.
-		    'self',
-		    // Allow loading from our assets domain.  Notice the difference between * and **.
-		    'http://localhost:8089/spark/**'
-		  ]);
+		$sceDelegateProvider.resourceUrlWhitelist([
+		// Allow same origin resource loads.
+		'self',
+		// Allow loading from our assets domain. Notice the difference between *
+		// and **.
+		'http://localhost:8089/spark/**' ]);
 
-		  // The blacklist overrides the whitelist so the open redirect here is blocked.
-		  $sceDelegateProvider.resourceUrlBlacklist([
-		    
-		  ]);
-		});
+		// The blacklist overrides the whitelist so the open redirect here is
+		// blocked.
+		$sceDelegateProvider.resourceUrlBlacklist([
+
+		]);
+	});
 	myapp.config([ '$routeProvider', function($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl : 'http://localhost:8089/spark/resources/Login.html',
@@ -85,9 +67,10 @@
 			controller : 'LoginController'
 		}).when('/home', {
 			templateUrl : 'http://localhost:8089/spark/resources/home.html',
-			controller : 'LoginController'
+			controller : 'LoginController',
+			access:true
 		}).when('/errorlogin', {
-			templateUrl : 'error.html'
+			templateUrl : 'http://localhost:8089/spark/resources/error.html'
 		}).when('/logout', {
 			templateUrl : 'http://localhost:8089/spark/resources/Login.html'
 		}).otherwise({
@@ -100,8 +83,9 @@
 					'LoginController',
 					[
 							'$scope',
-							'$http','$location',
-							function($scope, $http,$location) {
+							'$http',
+							'$location',
+							function($scope, $http, $location) {
 								$scope.user = {
 									j_username : "user",
 									j_password : "user"
@@ -129,7 +113,6 @@
 											});
 
 								};
-								
 
 								$scope.login = function() {
 
@@ -139,28 +122,33 @@
 												url : 'http://localhost:8089/spark/j_spring_security_check',
 												params : $scope.user,
 												contentType : 'application/json'
-											}).success(
-											function(data, status, headers,
-													config) {
-												if(status==200){
-												$location.path("/home");
-												access=true;
-												}
-												else{
-													$location.path("/errorlogin");
-												}
+											})
+											.success(
+													function(data, status,
+															headers, config) {
+														if (status == 200) {
+															$location
+																	.path("/home");
+															access = true;
+														} else {
+															$location
+																	.path("/errorlogin");
+														}
 
-											}).error(
-											function(data, status, headers,
-													config) {
-												console.log("error header",
-														headers);
-												console.log("error config",
-														config);
-												$location.path("/errorlogin");
-												
+													})
+											.error(
+													function(data, status,
+															headers, config) {
+														console.log(
+																"error header",
+																headers);
+														console.log(
+																"error config",
+																config);
+														$location
+																.path("/errorlogin");
 
-											});
+													});
 
 								};
 								$scope.logout = function() {
@@ -175,7 +163,7 @@
 											function(data, status, headers,
 													config) {
 												$location.path("/login");
-												access=false;
+												access = false;
 											}).error(
 											function(data, status, headers,
 													config) {
